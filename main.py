99359809f0efd4b3deb8c1d9ab5ccee62113c068
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-27 21:15:02 +0600

    First flip works

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import turtle
 from tkinter import *
 import pandas
 import random
+
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -12,12 +14,21 @@ to_learn = data_frame.to_dict(orient="records") #Changed the orient **
 print(to_learn) #prints a list consisting od dicts **
 print(type(to_learn)) #prints: list
 
+current_card = {}
+
 
 def next_card():
+    global current_card
     current_card = random.choice(to_learn) #lets say it picks 8th entry in list which is: {'Spanish': 'que', 'English': 'that'}
     print(current_card["Spanish"]) #the current_card["Spanish"] = "que", and current_card["English"] = "that"
     canvas.itemconfig(card_title, text="Spanish")
     canvas.itemconfig(card_word, text=current_card["Spanish"])
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_background, image=card_back_image)
+
+
 
 
 
@@ -26,15 +37,17 @@ def next_card():
 window = Tk()
 window.title("**Flashpoint**")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.after(3000, flip_card)
 
 
 #Canvas
 canvas = Canvas(height=600, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_image = PhotoImage(file="card_front.png")
-canvas.create_image(410, 300, image=card_front_image)
+card_back_image = PhotoImage(file="card_back.png")
+card_background = canvas.create_image(410, 300, image=card_front_image)
 canvas.grid(row=0, column=0, columnspan=2)
 card_title = canvas.create_text(380, 200, text="Title", font=("Arial", 40, "italic"))
-card_word = canvas.create_text(380, 320, text="Word", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(380, 320, text="Word", font=("Arial", 40, "bold"))
 
 
 
@@ -47,8 +60,8 @@ cross_image = PhotoImage(file="wrong.png")
 known_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
-#
-# next_card()
+
+next_card()
 
 
 
