e10cff93ec3083ed8156dd70217375a8114a2896
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-27 17:30:01 +0600

    Created canvas and added background image to it

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+#Window:
 window = Tk()
 window.title("**Flashpoint**")
+
+
+
+canvas = Canvas(height=600, width=800)
+logo_image = PhotoImage(file="card_front.png")
+canvas.create_image(400, 300, image=logo_image) #half of 200 is 100, so thats the center of the page
+canvas.grid()
+
+
 
 
 
