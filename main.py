6f678d6a66e883bd51cfb74a4140911c96d4c64a
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-27 17:32:57 +0600

    Changed bg color of canvas

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ window.config(padx=50, bg=BACKGROUND_COLOR)
 
 
 
-canvas = Canvas(height=700, width=800)
+canvas = Canvas(height=700, width=800, bg=BACKGROUND_COLOR)
 logo_image = PhotoImage(file="card_front.png")
 canvas.create_image(400, 300, image=logo_image) #half of 200 is 100, so thats the center of the page
 canvas.grid()
