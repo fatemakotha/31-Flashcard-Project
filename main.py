3bbd589c784608c12f0abe7967ba727f11782772
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-27 19:05:32 +0600

    Tied command to buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -9,32 +10,23 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 
 #Canvas
-canvas = Canvas(height=700, width=800, bg=BACKGROUND_COLOR, highlightthickness=1)
+canvas = Canvas(height=600, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_image = PhotoImage(file="card_front.png")
 canvas.create_image(410, 300, image=card_front_image)
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.create_text(380, 200, text="Title", font=("Arial", 40, "italic"))
 canvas.create_text(380, 320, text="Word", font=("Arial", 40, "italic"))
 
-# # Labels:
-# title_label = Label()
-# title_label.config(text="Title", font=("Arial", 40, "italic"))
-# title_label.place(x=340, y=150)
-#
-# word_label = Label()
-# word_label.config(text="Word", font=("Arial", 40, "bold"))
-# word_label.place(x=320, y=300)
 
 
-# #Buttons:
+#Buttons:
+check_image = PhotoImage(file="right.png")
+unknown_button = Button(image=check_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-right_image = PhotoImage(file="right.png")
-right_button = Button(image=right_image)
-right_button.grid(row=1, column=0)
-
-wrong_image = PhotoImage(file="wrong.png")
-wrong_button = Button(image=wrong_image)
-wrong_button.grid(row=1, column=1)
+cross_image = PhotoImage(file="wrong.png")
+known_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+known_button.grid(row=1, column=1)
 
 
 
