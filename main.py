ff1e21837a17d9ea223be12be5a7167221a2c1ef
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-27 17:58:03 +0600

    Tweaked width and height. All in shape now.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 
 #Canvas
-canvas = Canvas(height=700, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas = Canvas(height=700, width=800, bg=BACKGROUND_COLOR, highlightthickness=1)
 logo_image = PhotoImage(file="card_front.png")
-canvas.create_image(400, 300, image=logo_image) #half of 200 is 100, so thats the center of the page
+canvas.create_image(420, 300, image=logo_image)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Labels:
