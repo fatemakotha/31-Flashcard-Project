7c704932ceb479f193f2669ed86391d852514d1f
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-27 20:41:07 +0600

    Random card picked upon clicking buttons

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ print(type(to_learn)) #prints: list
 
 
 def next_card():
-    pass
+    current_card = random.choice(to_learn) #lets say it picks 8th entry in list which is: {'Spanish': 'que', 'English': 'that'}
+    print(current_card["Spanish"]) #the current_card["Spanish"] = "que", and current_card["English"] = "that"
+    canvas.itemconfig(card_title, text="Spanish")
+    canvas.itemconfig(card_word, text=current_card["Spanish"])
+
 
 
 
@@ -29,8 +33,8 @@ canvas = Canvas(height=600, width=800, bg=BACKGROUND_COLOR, highlightthickness=0
 card_front_image = PhotoImage(file="card_front.png")
 canvas.create_image(410, 300, image=card_front_image)
 canvas.grid(row=0, column=0, columnspan=2)
-canvas.create_text(380, 200, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(380, 320, text="Word", font=("Arial", 40, "italic"))
+card_title = canvas.create_text(380, 200, text="Title", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(380, 320, text="Word", font=("Arial", 40, "italic"))
 
 
 
@@ -43,8 +47,8 @@ cross_image = PhotoImage(file="wrong.png")
 known_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
-
-
+#
+# next_card()
 
 
 
