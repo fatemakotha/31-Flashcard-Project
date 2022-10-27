3e7be9218f8ad0cab86a2e057bcde7bf8a366aa1
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-27 17:32:08 +0600

    Changed background color of window

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ BACKGROUND_COLOR = "#B1DDC6"
 #Window:
 window = Tk()
 window.title("**Flashpoint**")
+window.config(padx=50, bg=BACKGROUND_COLOR)
 
 
 
-canvas = Canvas(height=600, width=800)
+canvas = Canvas(height=700, width=800)
 logo_image = PhotoImage(file="card_front.png")
 canvas.create_image(400, 300, image=logo_image) #half of 200 is 100, so thats the center of the page
 canvas.grid()
